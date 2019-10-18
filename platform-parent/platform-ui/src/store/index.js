68d47bb93bde7e87377412a9dacde49c64f91a4c
feng <46697915@qq.com>
2019-10-18 20:30:02 +0800

    增加加密算法工具类

--- platform-parent/platform-ui/src/store/index.js
+++ platform-parent/platform-ui/src/store/index.js
@@ -1,51 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import getters from './getters'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-    user: {
-      name: window.localStorage.getItem('user' || '[]') == null ? '未登录' : JSON.parse(window.localStorage.getItem('user' || '[]')).name,
-      userface: window.localStorage.getItem('user' || '[]') == null ? '' : JSON.parse(window.localStorage.getItem('user' || '[]')).userface,
-      username: window.localStorage.getItem('user' || '[]') == null ? '' : JSON.parse(window.localStorage.getItem('user' || '[]')).username,
-      roles: window.localStorage.getItem('user' || '[]') == null ? '' : JSON.parse(window.localStorage.getItem('user' || '[]')).roles
-    },
-    routes: [],
-    msgList: [],
-    isDotMap: new Map(),
-    currentFriend: {},
-    stomp: null,
-    nfDot: false
-  },
-  mutations: {
-    initMenu(state, menus){
-      state.routes = menus;
-    },
-    login(state, user){
-      state.user = user;
-      window.localStorage.setItem('user', JSON.stringify(user));
-    },
-    logout(state){
-      window.localStorage.removeItem('user');
-      state.routes = [];
-    },
-    toggleNFDot(state, newValue){
-      state.nfDot = newValue;
-    },
-    updateMsgList(state, newMsgList){
-      state.msgList = newMsgList;
-    },
-    updateCurrentFriend(state, newFriend){
-      state.currentFriend = newFriend;
-    },
-    addValue2DotMap(state, key){
-      state.isDotMap.set(key, "您有未读消息")
-    },
-    removeValueDotMap(state, key){
-      state.isDotMap.delete(key);
-    }
-  },
-  actions: {
-  }
-});
+// https://webpack.js.org/guides/dependency-management/#requirecontext
+//获取modules文件夹中module文件作为store的module
+const modulesFiles = require.context('./modules', true, /\.js$/)
+
+// you do not need `import app from './modules/app'`
+// it will auto require all vuex module from modules file
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+  // set './app.js' => 'app'
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles(modulePath)
+  modules[moduleName] = value.default
+  return modules
+}, {})
+
+const store = new Vuex.Store({
+  modules,
+  getters
+})
+
+export default store
